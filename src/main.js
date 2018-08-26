475f440db783e9e088144e96443ae54445351be0
Neko Stark <coderneko@gmail.com>
2018-08-27 06:04:45 +0800

    add psd test

--- src/main.js
+++ src/main.js
@@ -1,40 +1,12 @@
 'use strict'
-const {app, BrowserWindow, Notification, globalShortcut} = require("electron")
+let PSD = require("psd")
 
-let mainWindow
+let psd = PSD.fromFile("res/test.psd");
+psd.parse();
 
-function createWindow(){
-    globalShortcut.register("Ctrl+C", ()=>{
-        let myNotification = new Notification("Close", {
-            body : "Window close"
-        })
-
-        myNotification.onclick = ()=>{
-            console.log("clicked")
-        }
-    });
-
-    mainWindow = new BrowserWindow({
-        width: 300,
-        height: 300, 
-    });
-    mainWindow.loadFile('src/index.html');
-
-    mainWindow.on("closed", ()=>{
-        mainWindow = null;
-    });
-}
-
-app.on("ready", createWindow);
-
-app.on("activate", ()=>{
-    if(mainWindow === null){
-        app.quit();
-    }
-})
-
-app.on("window-all-closed", ()=>{
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+console.log(psd.tree().children()[0].export());
+PSD.open("res/test.psd").then(psd=>{
+    return psd.image.saveAsPng("res/test.png");
+}).then(()=>{
+    console.log("Finished!!")
 })
